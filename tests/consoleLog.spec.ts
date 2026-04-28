b2889470050919192a1dfa9cfedb10e71c5e5e49
Milos <milos.zak223@gmail.com>
2026-04-28 13:25:55 +0200

    Add consoleLog Playwright test

---
+++ tests/consoleLog.spec.ts
@@ -0,0 +1,24 @@
+import {test, expect} from '../fixtures/basePages';
+
+test.describe('Console log errors', () => {
+
+    test.only('Page has no error or logs', async ({page}) => {
+
+        const logs = [] as any;                                     // as any pole logs sa interpretuje ako pole do ktoreho mozu byt vlozene lubovolne typy hodnot
+        page.on('console', (message) => {                           //argument console ze budeme pocuvat na udalosti zobrazujuce spravy v konzole
+            return logs.push({ message, type: message.type()});     //vratime metodu logs.puhs ktory prida novy prvok do pola logs . dve polozky massage a type
+        });
+
+        const errors = [] as any; 
+        page.on ('pageerror', (exception) => {                         // kontrola na eroory
+            errors.push(exception);
+        });
+
+        await page.goto('https://www.aktuality.sk/');
+        console.log(logs);
+        expect.soft(logs.length).toBe(0);
+        console.log(errors);
+        expect(errors.length).toBe(0);
+        });
+    
+    });    
