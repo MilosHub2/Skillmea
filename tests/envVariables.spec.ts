52d00612b37be11f2264660781b0af422bafad1f
Milos <milos.zak223@gmail.com>
2026-05-05 19:09:54 +0200

    Projekt CI/CD

--- tests/envVariables.spec.ts
+++ tests/envVariables.spec.ts
@@ -1,6 +1,6 @@
 import {test} from '@playwright/test';
 
-test.only('Multiple enviroments', async ({page}) => {
+test('Multiple enviroments', async ({page}) => {
     console.log(process.env.E2E_BASE_URL);
     console.log(process.env.E2E_USERNAME);
     console.log(process.env.E2E_PASSWORD);
