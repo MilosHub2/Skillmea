1462eea962ab33b8e5c7bf0cf65f215e27df7dff
Milos <milos.zak223@gmail.com>
2026-05-07 19:32:07 +0200

    shard edit

---
+++ tests/api.spec.ts
@@ -0,0 +1,155 @@
+import { expect, test} from '@playwright/test';
+import { faker } from '@faker-js/faker';
+
+var token;
+
+test.describe('API tests @api', () => {
+
+    const randomFirstName = faker.person.firstName();
+    const randomLastName = faker.person.lastName();
+    const randomNumbner = faker.number.int(50);
+
+    test('Get request', async ({ request }) => {
+        const response = await request.get("https://restful-booker.herokuapp.com/booking/2") 
+        expect(response.status()).toBe(200);                        
+        const body = await response.json();
+        console.log(JSON.stringify(body)); // Prevedie javasceriptový objekt na řetězec JSON a vypíše ho do konzole
+    });
+
+    test('Get request with params', async ({ request}) => {
+        const response = await request.get("/booking", {
+            params: {
+                firstname: 'John', 
+                lastname: 'Smith'
+            },
+        });
+        expect(response.ok()).toBeTruthy();
+        expect(response.status()).toBe(200);
+        console.log(await response.json());
+    });
+
+    test('Post - create booking', async ({ request }) => {
+        const response = await request.post("/booking", {
+            data: {
+                "firstname" : "Jim",
+                "lastname" : "Brown",
+                "totalprice" : 111,
+                "depositpaid" : true,
+                    "bookingdates" : {
+                    "checkin" : "2018-01-01",
+                    "checkout" : "2019-01-01"
+                },
+                "additionalneeds" : "Breakfast"
+            }
+        });
+        console.log(await response.json());
+        expect(response.ok()).toBeTruthy();
+        expect(response.status()).toBe(200);
+        const responseBody = await response.json();
+        expect(responseBody.booking).toHaveProperty("firstname", "Jim");
+        expect(responseBody.booking).toHaveProperty("lastname", "Brown");
+        expect(responseBody.booking).toHaveProperty("totalprice", 111);
+    });
+
+    test('Post - dynamic data', async ({ request }) => {
+        const response = await request.post("/booking", {
+            data: {
+                "firstname" : randomFirstName,
+                "lastname" : randomLastName,
+                "totalprice" : randomNumbner,
+                "depositpaid" : true,
+                    "bookingdates" : {
+                    "checkin" : "2018-01-01",
+                    "checkout" : "2019-01-01"
+                },
+                "additionalneeds" : "Breakfast"
+            }
+        });
+        console.log(await response.json());
+        expect(response.ok()).toBeTruthy();
+        expect(response.status()).toBe(200);
+        const responseBody = await response.json();
+        expect(responseBody.booking).toHaveProperty("firstname", randomFirstName);
+        expect(responseBody.booking).toHaveProperty("lastname", randomLastName);
+        expect(responseBody.booking).toHaveProperty("totalprice", randomNumbner);
+    });
+
+    test('Update the booking detals', async ({ request }) => {
+        const response = await request.post("/auth", {
+            data: {
+                "username" : "admin",   
+                "password" : "password123"
+            }
+        });
+
+        console.log(await response.json());
+        expect(response.ok()).toBeTruthy();
+        expect(response.status()).toBe(200);
+        const responseBody = await response.json();
+        token = responseBody.token;
+        console.log("New toke in: " + token);
+
+        const updateRequest = await request.put("/booking/2", {
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                "Cookie": `token=${token}`,    // vzdy ked pouzivame nejaku premenu v kuceravych zatvorkach, tak sa musi pouzit backtick ` a nie obycejne navodniky ' alebo "
+            },
+            data: {
+                "firstname" : "Jozef",
+                "lastname" : "Žák",
+                "totalprice" : 66,
+                "depositpaid" : true,
+                    "bookingdates" : {
+                    "checkin" : "2018-01-01",
+                    "checkout" : "2019-01-01"
+                },
+                "additionalneeds" : "Breakfast"
+            }
+        });
+        
+        console.log(await updateRequest.json());
+        expect(updateRequest.ok()).toBeTruthy();
+        expect(updateRequest.status()).toBe(200);
+        const updatedResponseBody = await updateRequest.json();
+        console.log(updatedResponseBody);
+        expect(updatedResponseBody).toHaveProperty("firstname", "Jozef");
+        expect(updatedResponseBody).toHaveProperty("lastname", "Žák");
+        expect(updatedResponseBody).toHaveProperty("totalprice", 66);
+    });
+
+    test('Delete the booking detail', async ({ request }) => {
+        const response = await request.post("/auth", {
+            data: {
+                "username" : "admin",   
+                "password" : "password123"
+            }
+        });
+
+        console.log(await response.json());
+        expect(response.ok()).toBeTruthy();
+        expect(response.status()).toBe(200);
+        const responseBody = await response.json();
+        token = responseBody.token;
+        console.log("New toke in: " + token);
+
+        const deleteRequest = await request.delete("/booking/3", {
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                "Cookie": `token=${token}`,
+            },
+
+        });
+
+        expect(deleteRequest.status()).toBe(201);
+        expect(deleteRequest.statusText()).toBe("Created");
+    });
+
+    test.only('Block requests', async ({ page, context }) => {
+        await context.route(/\.(jpg|png|css)$/, route => route.abort());  //  await context.route s regex kod na blokovanie načítania obrázkov (jpg a png) a route.abort() pro zablokování těchto požadavků
+        await page.goto("/");                                           // kzada url s priponou.jpg alebo .png a tieto požadavky budou zablokovány, což může zrychlit načítání stránky a snížit spotřebu dat.
+        await page.waitForURL("/");
+    });
+
+});
