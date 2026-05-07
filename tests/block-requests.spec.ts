28eca6d83a88b7dfe2aea08c806b08b20b9d37e0
Milos <milos.zak223@gmail.com>
2026-05-07 20:53:40 +0200

    last try

--- tests/block-requests.spec.ts
+++ tests/block-requests.spec.ts
@@ -2,6 +2,6 @@ import { test } from '@playwright/test';
 
 test('Block requests', async ({ page, context }) => {
   await context.route(/\.(jpg|png|css)$/, route => route.abort());
-  await page.goto('/');
-  await page.waitForURL('/');
+  await page.goto('https://restful-booker.herokuapp.com');
+  await page.waitForURL('https://restful-booker.herokuapp.com');
 });
